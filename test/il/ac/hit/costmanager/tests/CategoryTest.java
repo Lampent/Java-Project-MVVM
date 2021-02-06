6f03c03ce54526227a80b08f74641d4f86886804
ronys <nrdronys@gmail.com>
2021-02-07 00:41:08 +0200

    comments on all

---
+++ test/il/ac/hit/costmanager/tests/CategoryTest.java
@@ -0,0 +1,98 @@
+
+package il.ac.hit.costmanager.tests;
+
+import il.ac.hit.costmanager.model.category.Category;
+import il.ac.hit.costmanager.exeptions.CostManagerException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+/**
+ * Test for the category class.
+ */
+public class CategoryTest {
+
+    public CategoryTest() {
+    }
+
+    /**
+     * Test invalid category creation.
+     */
+    @Test
+    public void testInvalidCategoryCreation() {
+        Category category = null;
+
+        // tests that cannot create category with empty name
+        try {
+            category = new Category("", 100);
+        } catch (CostManagerException e) {
+
+        }
+        assertNull(category);
+
+        // tests that cannot create category with negative id
+        try {
+            category = new Category("", -200);
+        } catch (CostManagerException e) {
+
+        }
+        assertNull(category);
+    }
+
+    /**
+     * Test valid category creation.
+     */
+    @Test
+    public void testValidCategoryCreation() throws CostManagerException {
+        String categoryName = "Fee";
+        int categoryId = 100;
+
+        // tests first constructor
+        Category firstCategory = new Category(categoryName);
+        firstCategory.setCategoryName(categoryName);
+        assertEquals(categoryName, firstCategory.getCategoryName());
+
+        // tests second constructor
+        Category secondCategory = new Category(categoryName, categoryId);
+        secondCategory.setCategoryName(categoryName);
+        secondCategory.setId(categoryId);
+        assertEquals(categoryName, secondCategory.getCategoryName());
+        assertEquals(categoryId, secondCategory.getId());
+    }
+
+    /**
+     * Test of category name.
+     */
+    @Test
+    public void testCategoryName() throws CostManagerException {
+        String startCategoryName = "Start name";
+        String categoryName = "End name";
+
+        // checks the set of the category name in constructor
+        Category category = new Category(startCategoryName);
+        assertEquals(startCategoryName, category.getCategoryName());
+
+        // checks the set of the category name over setter
+        category.setCategoryName(categoryName);
+        assertEquals(categoryName, category.getCategoryName());
+    }
+
+    /**
+     * Test of category id.
+     */
+    @Test
+    public void testCategoryId() throws CostManagerException {
+        int startId = 100;
+        int endId = 200;
+
+        // checks the set of the category id in constructor
+        Category category = new Category("test", startId);
+        assertEquals(startId, category.getId());
+
+        // checks the set of the category id over setter
+        category.setId(endId);
+        assertEquals(endId, category.getId());
+    }
+}
---
+++ test/il/ac/hit/costmanager/tests/CostTest.java
@@ -0,0 +1,104 @@
+
+package il.ac.hit.costmanager.tests;
+
+import il.ac.hit.costmanager.model.cost.Cost;
+import il.ac.hit.costmanager.exeptions.CostManagerException;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Tests for the cost class.
+ */
+public class CostTest {
+
+    public CostTest() {
+    }
+
+    /**
+     * Test of getCategory method, of class Cost.
+     */
+    @Test
+    public void testGetCategory() throws CostManagerException {
+        String category = "Grocery";
+        Cost cost = new Cost(category, "30-10-1997", "test description", 1);
+        assertEquals(cost.getCategory(), category);
+    }
+
+    /**
+     * Test of setCategory method, of class Cost.
+     */
+    @Test
+    public void testSetCategory() throws CostManagerException {
+        String category = "Grocery";
+        Cost cost = new Cost("test category", "30-10-1997", "test description", 1);
+        cost.setCategory(category);
+        assertEquals(category, cost.getCategory());
+    }
+
+    /**
+     * Test of getDate method, of class Cost.
+     */
+    @Test
+    public void testGetDate() throws CostManagerException {
+        String date = "30-10-2000";
+        Cost cost = new Cost("test category", date, "test description", 1);
+        assertEquals(cost.getDate(), date);
+    }
+
+    /**
+     * Test of setDate method, of class Cost.
+     */
+    @Test
+    public void testSetDate() throws CostManagerException {
+        String date = "30-10-2000";
+        Cost cost = new Cost("test category", "30-10-1997", "test description", 1);
+        cost.setDate(date);
+        assertEquals(cost.getDate(), date);
+    }
+
+    /**
+     * Test of getDesc method, of class Cost.
+     */
+    @Test
+    public void testGetDesc() throws CostManagerException {
+        String description = "Description test";
+        Cost cost = new Cost("test category", "30-10-1997", description,1);
+        assertEquals(cost.getDesc(), description);
+    }
+
+    /**
+     * Test of setDesc method, of class Cost.
+     * @throws CostManagerException
+     */
+    @Test
+    public void testSetDesc() throws CostManagerException {
+        String description = "Description test";
+        Cost cost = new Cost("test category", "30-10-1997", "test description", 1);
+        cost.setDesc(description);
+        assertEquals(cost.getDesc(), description);
+    }
+
+    /**
+     * Test of getCost method, of class Cost.
+     */
+    @Test
+    public void testGetCost() throws CostManagerException {
+        double costTest = 100;
+        Cost cost = new Cost("test category", "30-10-1997", "test description", costTest);
+        assertEquals(costTest, cost.getCost(), 0);
+    }
+
+    /**
+     * Test of setCost method, of class Cost.
+     */
+    @Test
+    public void testSetCost() throws CostManagerException {
+        double costTest = 100;
+        Cost cost = new Cost("test category", "30-10-1997", "test description", 1);
+        cost.setCost(costTest);
+        assertEquals(costTest, cost.getCost(), 0);
+    }
+
+}
